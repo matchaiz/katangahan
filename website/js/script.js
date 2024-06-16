ed59c436d48805ebb72af7c179ef94172666dffe
matchaiz <lorenjoyce9@gmail.com>
2024-06-16 19:42:21 +0800

    Add files via upload

---
+++ website/js/script.js
@@ -0,0 +1,31 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text',{delay:200, origin:'top'});
+sr.reveal('.hero-img',{delay:450, origin:'top'});
+sr.reveal('.icons',{delay:500, origin:'left'});
+
+var icon = document.getElementById("moon-icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        icon.src = "sun.png";
+    }else{
+       icon.src = "moon.png";
+    }
+}
+
+
